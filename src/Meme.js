217a8dad2bfda600b5fa2fcf5a7e690d813ff73b
Dylan Bailey <wearesleepless@gmail.com>
2023-10-04 10:31:07 -0400

    Form Component

---
+++ src/Meme.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Meme() {
+    return(
+        <form className="meme--form">
+            <div className="meme-form-inputs">
+                <input type="text" name="" id="" className="meme--form-input"/>
+                <input type="text" name="" id="" className="meme--form-input"/>
+            </div>
+            <button className="meme--form-btn">Get a new meme image 🖼️</button>
+        </form>
+    );
+}
+
+export default Meme;
